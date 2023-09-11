3a985469d550aef8ac7a0fcf10cba840a403194a
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-11 15:30:28 -0300

    Adicionar arquivos de projeto.

---
+++ AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_Solutech {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions options) : base(options) {
+        }
+    }     
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_Solutech {
+    public class Program {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var _mySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 33));
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => {
+                    options.UseMySql(
+                       builder.Configuration.GetConnectionString("DBString"),
+                        _mySqlServerVersion
+
+                       );
+                }
+             );
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
