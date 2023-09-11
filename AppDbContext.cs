cd8003d159596154f3fb62d27a8f5129a52addd7
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-11 17:55:29 -0300

    Add atributos a classe produto

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ecommerce_Solutech.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ecommerce_Solutech {
     public class AppDbContext : DbContext {
         public AppDbContext(DbContextOptions options) : base(options) {
         }
+           public DbSet <Produto> produtos { get; set; }
     }     
 }
