3be568a5a96f1573fbb125e76ad4958bbb0b4ed7
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-12 15:21:05 -0300

    Add  nova classe Produtos

---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_Solutech.Models {
+    public class Produto {
+        [Key]
+        public int? id { get; set; }
+        public string Nome { get; set; }
+        public float Estoque { get; set; }
+        public double Preco { get; set; }
+        public float EstoqueMax { get; set; }
+        public float EstoqueMedio { get; set; }
+        public float EstoqueMin { get; set; }
+        public DateTime VencimentoProduto { get; set; }
+        public double preco { get; set; }
+
+
+    }
+}
