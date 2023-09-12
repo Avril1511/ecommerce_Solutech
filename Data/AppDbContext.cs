274960e777092641afd777defbe66454de41fe98
Ewerton <antonio48241686@edu.pa.senac.br>
2023-09-12 15:44:53 -0300

    Removel nomes de branch que não devia estár

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,22 +1,17 @@
- Add-Class-Cliente
+ 
 ﻿using ecommerce_Solutech.Pages;
 ﻿using ecommerce_Solutech.Models;
-master
 using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce_Solutech {
     public class AppDbContext : DbContext {
-        public AppDbContext(DbContextOptions options) : base(options) {
+        public AppDbContext(DbContextOptions options) : base(options){
         }
- Add-Class-Cliente
         public DbSet<Cliente> cliente { get; set; }
         public DbSet<Endereco> endereco { get; set; }
-       public DbSet<Produto> produtos { get; set; }
-       
-       public DbSet<Endereco> enderecos { get; set; }
+        public DbSet<Produto> produtos { get; set; }
 
-
-
-master
+        public DbSet<Endereco> enderecos { get; set; }
+ 
     }     
 }
