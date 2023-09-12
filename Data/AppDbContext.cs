784d6a33569f44479fde27f6a2646a422f291782
Ewerton <antonio48241686@edu.pa.senac.br>
2023-09-12 15:30:13 -0300

    Adicionando os códigos da classe cliente

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ecommerce_Solutech.Pages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce_Solutech {
     public class AppDbContext : DbContext {
         public AppDbContext(DbContextOptions options) : base(options) {
         }
+        public DbSet<Cliente> cliente { get; set; }
+        public DbSet<Endereco> endereco { get; set; }
     }     
 }
