a34439c1d23d5dd154e385b6126740bd2c4820f9
João Paulo <joao11283886@edu.pa.senac.br>
2023-09-12 16:23:39 -0300

    tá aqui o de hoje

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ecommerce_Solutech.Pages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce_Solutech {
     public class AppDbContext : DbContext {
         public AppDbContext(DbContextOptions options) : base(options) {
         }
+        public DbSet<>
     }     
 }
