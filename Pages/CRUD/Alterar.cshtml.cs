ab27b4dd7c46577576b5efcc79c4e1a00c3f125f
Ewerton <antonio48241686@edu.pa.senac.br>
2023-09-22 17:51:07 -0300

    add códigos e criando a pasta alterar razor e fazendo alterações dentro de cliente

---
+++ Pages/CRUD/Alterar.cshtml.cs
@@ -0,0 +1,38 @@
+using ecommerce_Solutech.Data;
+using ecommerce_Solutech.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_Solutech.Pages.CRUD{
+    public class AlterarModel : PageModel{
+        private readonly AppDbContext _context;
+
+        public AlterarModel(AppDbContext context){
+            _context = context;
+        }
+
+        /*
+         O '[BindProperty]' configura a aplicação para relacionar o atributo
+        'cliente' aos dados que estão vindo do front-end
+        */
+        [BindProperty]
+        public Cliente cliente { get; set; }
+
+        public async Task<IActionResult> OnGet(int id) {
+            cliente = await _context.cliente.FirstOrDefaultAsync (c => c.Id ==id);
+
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(){
+            _context.Attach(cliente).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Listar");
+    
+
+        }
+    }
+}
