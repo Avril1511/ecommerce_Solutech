f3b770c578d81f22a3422a3bd4353e0db142b3b7
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-26 17:54:51 -0300

    Add o Data Annotetion

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -3,14 +3,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ecommerce_Solutech.Models {
     public class Endereco {
-        
+        [Key]
         public int Id { get; set; }
-        public string Cep { get; set; }
-        public string NomeCidade { get; set; }
-        public string Municipio { get; set; }
-        public string NomeRua {  get; set; }
-        public string Bairro { get; set; }
-        public string Estado { get; set; }
+		[RegularExpression(@"[0-9]{8}", ErrorMessage = "O campo {Endereço} deve ser preenchido com 8 digitos numéricos")]
+		public string Cep { get; set; }
+		[MaxLength(100, ErrorMessage = "O campo {Nome da Cidade} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {Nome da Cidade} deve ter no mínimo {1} caracteres.")]
+		public string NomeCidade { get; set; }
+		[MaxLength(100, ErrorMessage = "O campo {Municipio} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {Municipio} deve ter no mínimo {1} caracteres.")]
+
+		public string Municipio { get; set; }
+		[MaxLength(100, ErrorMessage = "O campo {Nome da Rua} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {Nome da Rua} deve ter no mínimo {1} caracteres.")]
+
+		public string NomeRua {  get; set; }
+		[MaxLength(100, ErrorMessage = "O campo {Bairro} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {Bairro} deve ter no mínimo {1} caracteres.")]
+
+		public string Bairro { get; set; }
+		[MaxLength(100, ErrorMessage = "O campo {Estado} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {Estado} deve ter no mínimo {1} caracteres.")]
+
+		public string Estado { get; set; }
         public string NumerodaCasa { get; set; }
         [ForeignKey("IdEndereco")]
         public Endereco endereco { get; set; }
