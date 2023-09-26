08c28aea3bbaab2391805dd2a9cada4ff8d7dba9
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-26 17:39:54 -0300

    Add Data Annotation

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -5,10 +5,13 @@ namespace ecommerce_Solutech.Models {
     public class Produto {
         [Key]
         public int? Id { get; set; }
-        public string Nome { get; set; }
+		[MaxLength(100, ErrorMessage = "O campo {Nome} Deve ter no Máximo {1}")]
+		[MinLength(50, ErrorMessage = "O campo {Nome} Deve ter no Mínimo {1} caracteres.")]
+		public string Nome { get; set; }
         public float Estoque { get; set; }
         public double Preco { get; set; }
-        public DateTime VencimentoProduto { get; set; }
+		[DataType(DataType.Date, ErrorMessage = "O campo {Vencimento do Produto} deve ter uma data válida")]
+		public DateTime VencimentoProduto { get; set; }
         public ICollection<ItemDoPedido> ItemDoPedidos { get; set; }
     }
 }
