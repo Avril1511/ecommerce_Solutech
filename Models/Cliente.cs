bba6819a0c47e941f6750cb5f6cf97bb867f45cf
Ewerton <antonio48241686@edu.pa.senac.br>
2023-09-27 15:45:56 -0300

    Add Data Annotetion nos seguintes códigos para a classe cliente mensagens de erros e prenchimento obrigatorio

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -8,28 +8,42 @@ using System.Diagnostics.CodeAnalysis;
 namespace ecommerce_Solutech.Models{
     public class Cliente{
         [Key]
-
         public int? Id { get; set; }
-		[Required(ErrorMessage = "0 campos {0} é de preenchimento obrigatório")]
-		[MaxLength(100)]
+
 		public int? IdEndereco { get; set; }
-        public string Nome { get; set; }
+
+
+		[Required(ErrorMessage = "0 campos {0} é de preenchimento obrigatório")]
+		public string Nome { get; set; }
+
+
         [Required(ErrorMessage = "0 campo \"{0}\" é de preenchimento obrigatório")]
-        [DataType(DataType.Date, ErrorMessage = "0 campo {0} de ter uma data")]
-        [DisplayName("Data de Nascimento")]
-        public string email { get; set; }
-        public string telefone { get; set;}
-        
+        [EmailAddress(ErrorMessage = "O campo \"{0}\" é de prenchimento obrigatório")]
+		[DisplayName("E-mail")]
+		public string email { get; set; }
+
+		[Required(ErrorMessage = "0 campo \"{0}\" é de preenchimento obrigatório")]
+		[DataType(DataType.Date, ErrorMessage = "0 campo {0} de ter uma data")]
+		[DisplayName("Data de Nascimento")]
+		public DateTime DataNascimento { get; set; }
+
+		
+		[MinLength(11, ErrorMessage = "O campo \"{0}\" deve conter no máximo {1} caracteres.")]
+        [DisplayName("Telefone")]
+
+		public string telefone { get; set;}
+
+
         [Required(ErrorMessage = "O campo \"{0}\" é de prenchimento obrigatório.")]
-        [DisplayName("email")]
-        [EmailAddress(ErrorMessage = "O campo \"{0}\"deve conter um endereço de email válido.")]
-        [MaxLength(50, ErrorMessage = "O campo \"{0}\" deve conter no máximo {1} caracteres.")]
-
- 
+        [MaxLength(50, ErrorMessage = "O campo \"{0}\" deve conter no minimo {8} caracteres.")]
+		[DisplayName("Login")]
         public string login { get; set;}
-        public string HashSenha { get; set;}
 
-        [ForeignKey("IdEndereco")]
+		[MinLength(8, ErrorMessage = "O campo {0} deve ter no mínimo {6} caracteres.")]
+		[DisplayName("HasSenha")]
+		public string HashSenha { get; set;}
+
+
         public Endereco? Endereco { get; set; }
         public ICollection<Pedido> Pedidos { get; set; }
 
