b7e2dc7f264b068feb4af25704a5317f939cbc8e
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-27 15:14:26 -0300

    Add Data Annotation

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -5,27 +5,39 @@ namespace ecommerce_Solutech.Models {
     public class Endereco {
         [Key]
         public int Id { get; set; }
+
+
 		[RegularExpression(@"[0-9]{8}", ErrorMessage = "O campo {Endereço} deve ser preenchido com 8 digitos numéricos")]
 		public string Cep { get; set; }
-		[MaxLength(100, ErrorMessage = "O campo {Nome da Cidade} deve ter tamanho igual a {1}")]
-		[MinLength(50, ErrorMessage = "O campo {Nome da Cidade} deve ter no mínimo {1} caracteres.")]
+
+		[Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+		[MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		[Display(Name = "Nome da Cidade")]
 		public string NomeCidade { get; set; }
-		[MaxLength(100, ErrorMessage = "O campo {Municipio} deve ter tamanho igual a {1}")]
-		[MinLength(50, ErrorMessage = "O campo {Municipio} deve ter no mínimo {1} caracteres.")]
 
+		[Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+		[MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
 		public string Municipio { get; set; }
-		[MaxLength(100, ErrorMessage = "O campo {Nome da Rua} deve ter tamanho igual a {1}")]
-		[MinLength(50, ErrorMessage = "O campo {Nome da Rua} deve ter no mínimo {1} caracteres.")]
 
+		[Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+		[MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		[Display(Name = "Nome da Rua")]
 		public string NomeRua {  get; set; }
-		[MaxLength(100, ErrorMessage = "O campo {Bairro} deve ter tamanho igual a {1}")]
-		[MinLength(50, ErrorMessage = "O campo {Bairro} deve ter no mínimo {1} caracteres.")]
 
+		[Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+		[MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+		[MinLength(50, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
 		public string Bairro { get; set; }
-		[MaxLength(100, ErrorMessage = "O campo {Estado} deve ter tamanho igual a {1}")]
-		[MinLength(50, ErrorMessage = "O campo {Estado} deve ter no mínimo {1} caracteres.")]
 
+		[Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+		[MaxLength(2, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+		[MinLength(2, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
 		public string Estado { get; set; }
+
+		[Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
         public string NumerodaCasa { get; set; }
         [ForeignKey("IdEndereco")]
         public Endereco endereco { get; set; }
