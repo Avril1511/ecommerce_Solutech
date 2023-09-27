3a4a86c00d42371b053971a08ca0d78db79f4b5b
João Paulo <joao11283886@edu.pa.senac.br>
2023-09-27 16:12:08 -0300

    Adição dos Data Annotetion

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -5,14 +5,15 @@ namespace ecommerce_Solutech.Models {
     public class Pedido {
         [Key]
         public int id { get; set; }
-
+        
         public int IdEndereco { get; set; }
 
         public int IdCliente { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public double valorTotal { get; set; }
 
-        public DateTime DataPedido { get; set; }
+        public DateTime DataPedido { get; set; } = DateTime.Now;
 
         [ForeignKey("IdCliente")]
         public Cliente cliente { get; set;}
