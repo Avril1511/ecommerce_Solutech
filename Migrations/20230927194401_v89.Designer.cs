78d29458381289b332924910a5c442f0172a11e0
Avril1511 <avril48340866@edu.pa.senac.br>
2023-09-27 16:45:56 -0300

    Add da imolemantação Data de Nascimento na Classe Clientre

--- Migrations/20230927194401_v89.Designer.cs
+++ Migrations/20230927194401_v89.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ecommerce_Solutech.Data;
 
@@ -10,9 +11,11 @@ using ecommerce_Solutech.Data;
 namespace ecommerce_Solutech.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230927194401_v89")]
+    partial class v89
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230927194401_v89.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce_Solutech.Migrations
+{
+    /// <inheritdoc />
+    public partial class v89 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_cliente_Endereco_IdEndereco",
+                table: "cliente");
+
+            migrationBuilder.DropIndex(
+                name: "IX_cliente_IdEndereco",
+                table: "cliente");
+
+            migrationBuilder.RenameColumn(
+                name: "quantidade",
+                table: "itemDoPedido",
+                newName: "quantidadepedido");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "produtos",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeRua",
+                table: "Endereco",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeCidade",
+                table: "Endereco",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Municipio",
+                table: "Endereco",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Endereco",
+                type: "varchar(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Endereco",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "login",
+                table: "cliente",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataNascimento",
+                table: "cliente",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "EnderecoId",
+                table: "cliente",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cliente_EnderecoId",
+                table: "cliente",
+                column: "EnderecoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cliente_Endereco_EnderecoId",
+                table: "cliente",
+                column: "EnderecoId",
+                principalTable: "Endereco",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_cliente_Endereco_EnderecoId",
+                table: "cliente");
+
+            migrationBuilder.DropIndex(
+                name: "IX_cliente_EnderecoId",
+                table: "cliente");
+
+            migrationBuilder.DropColumn(
+                name: "DataNascimento",
+                table: "cliente");
+
+            migrationBuilder.DropColumn(
+                name: "EnderecoId",
+                table: "cliente");
+
+            migrationBuilder.RenameColumn(
+                name: "quantidadepedido",
+                table: "itemDoPedido",
+                newName: "quantidade");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "produtos",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeRua",
+                table: "Endereco",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeCidade",
+                table: "Endereco",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Municipio",
+                table: "Endereco",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Endereco",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(2)",
+                oldMaxLength: 2)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Endereco",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "login",
+                table: "cliente",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cliente_IdEndereco",
+                table: "cliente",
+                column: "IdEndereco");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cliente_Endereco_IdEndereco",
+                table: "cliente",
+                column: "IdEndereco",
+                principalTable: "Endereco",
+                principalColumn: "Id");
+        }
+    }
+}
