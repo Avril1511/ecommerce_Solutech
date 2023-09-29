4ff1bad115cf3e1443faf14eaffe8bb3db8ee164
Ewerton <antonio48241686@edu.pa.senac.br>
2023-09-29 17:53:02 -0300

    Add de códigos na pagina incluir.cshtml.cs

--- Pages/ProdutoCRUD/Incluir.cshtml.cs
+++ Pages/ProdutoCRUD/Incluir.cshtml.cs
@@ -2,10 +2,13 @@ using ecommerce_Solutech.Data;
 using ecommerce_Solutech.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ecommerce_Solutech.Pages.ProdutoCRUD {
 	public class IncluirModel : PageModel
-    {
+	{
 		private readonly AppDbContext _context;
 
 		public IncluirModel(AppDbContext context) {
@@ -15,27 +18,46 @@ namespace ecommerce_Solutech.Pages.ProdutoCRUD {
 		[BindProperty]
 		public Produto produto { get; set; }
 
+
+
+		private readonly IWebHostEnvironment _webHostEnvironment;
+
+		public string CaminhoImagem { get; set; }
+
+		[BindProperty]
+		[DisplayName("Imagem do Produto")]
+		[Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatorio.")]
+
+		public IFormFile ImagemProduto { get; set; }
+
+		public IncluirModel(AppDbContext context, IWebHostEnvironment whe)
+		{
+			_context = context;
+			_webHostEnvironment = whe;
+			CaminhoImagem = "/img/produto/sem_imagem.jpg";
+
+		}
+
 		public void OnGet() {
 		}
 		public async Task<IActionResult> OnPostAsync() {
 			var produto = new Produto();
 
-			await TryUpdateModelAsync<Produto>(
-					produto,
-					"produto",
-					o => o.Nome, o => o.Preco, o => o.Estoque,o => o. VencimentoProduto
-					);
+			bool formValidado = await TryUpdateModelAsync<Produto>(
+									produto,
+									"produto",
+									o => o.Nome, o => o.Preco, o => o.Estoque,o => o. VencimentoProduto
+								);
 
-			_context.produtos.Add(produto);
+			if ( formValidado) { 
+				_context.produtos.Add(produto);
+				await _context.SaveChangesAsync();
 
-			await _context.SaveChangesAsync();
+				return RedirectToPage("./Listar");
+			}
 
 			return Page();
-
-			//return RedirectToPage("./Listar");
-
-
-
+			
 		}
 	}
 }
