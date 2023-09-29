bee25e3f355441e17ce4ace2e01796b3ed8d2da9
Cyranto Filho <cyranto10005866@edu.pa.senac.br>
2023-09-29 17:57:39 -0300

    Add designr do produto e o preço

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ecommerce_Solutech.Data;
+using ecommerce_Solutech.Models;
+using ecommerce_Solutech.Pages.CRUD;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace ecommerce_Solutech.Pages {
-    public class IndexModel : PageModel {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger) {
-            _logger = logger;
+namespace ecommerce_Solutech.Pages{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _context;
+        public IndexModel(AppDbContext context)
+        {
+            _context = context;
         }
+        public IList<Produto> Produtos { get; set; }
 
-        public void OnGet() {
+
+        public async Task<IActionResult> OnGet()
+        {
+            Produtos = await _context.produtos.ToListAsync();
+
+            return Page();
 
         }
     }
