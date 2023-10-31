e39bc3e591cf217432da3b3e1d7628bb7a8dbe46
João Paulo <joao11283886@edu.pa.senac.br>
2023-10-31 18:04:05 -0300

    Ajuste de conflito

--- Pages/ProdutoCRUD/Alterar.cshtml.cs
+++ Pages/ProdutoCRUD/Alterar.cshtml.cs
@@ -18,6 +18,15 @@ namespace ecommerce_Solutech.Pages.CRUD.ProdutoCRUD
         [BindProperty]
 
         public Produto produto { get; set; }
+        public string CaminhoImagem { get; set; }
+        [BindProperty]
+        [Display(Name = "Imagem do Produto")]
+        public IFormFile ImagemProduto { get; set; }
+
+        public AlterarModel(AppDbContext context, IWebHostEnvironment webHostEnvironment) {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
 
         public async Task<IActionResult> OnGet(int id) {
             produto = await _context.produtos.FirstOrDefaultAsync(c => c.Id == id);
@@ -26,10 +35,36 @@ namespace ecommerce_Solutech.Pages.CRUD.ProdutoCRUD
         }
 
         public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
             _context.Attach(produto).State = EntityState.Modified;
-            await _context .SaveChangesAsync();
-           
-            return Page();
+
+            try {
+                await _context.SaveChangesAsync();
+                //Se há uma imaem de produtosubmetida
+                if (ImagemProduto != null)
+                    await AppUtils.ProcessarArquivoDeImagem(
+                        produto.Id,
+                        ImagemProduto,
+                        _webHostEnvironment
+                );
+
+            } catch (DbUpdateConcurrencyException error) {
+                if (!ProdutoAindaNãoExiste(produto.Id)) {
+                    return NotFound();
+                } else {
+                    throw;
+                }
+
+            } catch {
+                return Page();
+            }
+            return RedirectToPage("./Listar");
+        }
+
+        private bool ProdutoAindaNãoExiste(int? id) {
+            throw new NotImplementedException();
         }
     }
 }
